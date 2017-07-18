5be476d87e7512b8902bf176ed3496e11c8a8795
Jesse Bryan <jesse.bryan22@gmail.com>
2017-07-18 11:45:35 -0700

    add JS functionality for display and multiple tickets

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,7 @@
 function Ticket(movie, time, age, isNew) {
   this.movie = movie;
   this.time = time;
-  this.price = getTicketPrice(isNew, time, age);
+  this.price = parseInt(getTicketPrice(isNew, time, age));
   var screenNumber;
   if (isNew) {
     screenNumber = Math.ceil(Math.random() * 3);
@@ -13,12 +13,16 @@ function Ticket(movie, time, age, isNew) {
   this.screen = screenNumber;
 }
 
+Ticket.prototype.getShoppingCartInfo = function() {
+  return this.movie + ' $' + this.price;
+}
+
 Ticket.prototype.getFullInfo = function(){
   return this.movie + ' in theater' + this.screen + ' at ' + this.time + '. Paid: $' + this.price;
 }
 
 function getTicketPrice(isNew, time, age) {
-  var price = 14;
+  var price = 15;
   if (isMatinee(time)) {
     price -= 3;
   }
@@ -39,6 +43,8 @@ function isMatinee(time) {
   }
 }
 
+var ticketPrices = [];
+
 //UI Logic
 
 $(function(){
@@ -48,7 +54,32 @@ $(function(){
     var inputtedTime = $("#time").val();
     var inputtedAge = $("#age").val();
     var inputtedIsNew = $("#movie option:selected").hasClass('new');
+    var total = 0;
 
     var newTicket = new Ticket(inputtedMovie, inputtedTime, inputtedAge, inputtedIsNew);
+    total += newTicket.price
+    // ticketPrices.push(newTicket.getPrice());
+
+    $('.ticket-display-list ul').append('<li><span class="cartItem">' + newTicket.getShoppingCartInfo() + '</span><span class="removeItem"> Remove from cart</span></li>');
+    $('.price').text(total);
+
+    $(".ticket-display-list").show();
+    $(".price-total").show();
+
+    $(".cartItem").last().click(function(){
+      $(".ticket-full-display h3").text(newTicket.movie);
+      $(".full-display-time").text(newTicket.time);
+      $(".full-display-screen").text(newTicket.screen);
+      $(".full-display-price").text(newTicket.price);
+      $(".ticket-full-display").show();
+    });
+    $(".removeItem").last().click(function(){
+      this.parentNode.remove();
+      total -= this.previousSibling
+      $('.price').text(total);
+    })
+  });
+  $(".purchase").submit(function(event){
+
   });
 });
